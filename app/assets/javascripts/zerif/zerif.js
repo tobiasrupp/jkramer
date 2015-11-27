1f897d57b54a3b8a8088956bce6abb5dfb1a7bfc
Tobias Rupp <tobias.rupp@gmx.com>
2015-11-27 22:57:15 +0100

    Cleaned up about page. Implemented Inquiry form.

--- app/assets/javascripts/zerif/zerif.js
+++ app/assets/javascripts/zerif/zerif.js
@@ -125,9 +125,11 @@ $("#contact-form").submit(function (e) {
     e.preventDefault();
     var name = $("#name").val();
     var email = $("#email").val();
-    var subject = $("#subject").val();
+    var phone = $("#phone").val();
     var message = $("#message").val();
-    var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
+    var AUTH_TOKEN = $('meta[name=csrf-token]').attr('content');
+    // var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message + '&authenticity_token=' + AUTH_TOKEN;
+    var params = {inquiry: {name: name, email: email, phone: phone, message: message}, authenticity_token: AUTH_TOKEN};
 
     function isValidEmail(emailAddress) {
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
@@ -137,8 +139,8 @@ $("#contact-form").submit(function (e) {
     if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
         $.ajax({
             type: "POST",
-            url: "sendmail.php",
-            data: dataString,
+            url: "contact",
+            data: params,
             success: function () {
                 $('.success').fadeIn(1000);
                 $('.error').fadeOut(500);
