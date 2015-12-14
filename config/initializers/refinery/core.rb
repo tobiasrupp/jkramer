f22f3b94ea392bae4e19a16a383e128f1d8ca2d7
Tobias Rupp <tobias.rupp@gmx.com>
2015-12-14 20:56:44 +0100

    Fix for S3 image upload issue.

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -16,7 +16,7 @@ Refinery::Core.configure do |config|
   # When true will use Amazon's Simple Storage Service instead of
   # the default file system for storing resources and images
   config.s3_backend = config.s3_access_key_id.present? || config.s3_secret_access_key.present?
-
+  config.s3_backend = false
   # Use a custom Dragonfly storage backend instead of the default
   # file system for storing resources and images
   # config.dragonfly_custom_backend_class = nil
